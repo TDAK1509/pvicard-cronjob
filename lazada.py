b84efc5d51100edef50b9ae51285807ef83319be
Khuong Tran <dbcgdhh@gmail.com>
2021-05-23 16:07:43 +0700

    Handle synco rder response in main

--- lazada.py
+++ lazada.py
@@ -1,5 +1,6 @@
 import os
 
+from http import HTTPStatus
 from models.lazada.logger import LazadaLogger
 from models.lazada.lazada import Lazada as LazadaApi
 from models.lazada.pvicard import PviCard
@@ -31,9 +32,23 @@ def main():
         pvi_card = PviCard(PVICARD_API_TOKEN)
 
         for order in pending_orders:
-            pvi_card.sync_orders(**order)
+            response = pvi_card.sync_orders(**order)
+            handle_sync_order_response(order["order_number"], response)
     except Exception as e:
         logger.exception(str(e))
+
+
+def handle_sync_order_response(order_number: str, request):
+    request_body = request.json()
+    status_code = request.status_code
+    message = request_body.get("message", "Empty error message.")
+
+    if status_code == HTTPStatus.OK:
+        logger.info(f"Order {order_number} is successfully processed.")
+    else:
+        logger.error(
+            f"Order {order_number} failed with code {status_code}. Error message: {message}"
+        )
 
 
 if __name__ == "__main__":
