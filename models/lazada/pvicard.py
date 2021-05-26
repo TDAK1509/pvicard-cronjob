dc8a2f17a58b3398cfc1603c71d4d249c5d001ed
Khuong Tran <dbcgdhh@gmail.com>
2021-05-26 19:46:48 +0700

    Add logging to pvicard class

--- models/lazada/pvicard.py
+++ models/lazada/pvicard.py
@@ -1,5 +1,10 @@
 import requests
 
+from models.lazada.logger import LazadaLogger
+
+
+logger = LazadaLogger().get_logger(__name__)
+
 
 class PviCard:
     def __init__(self, api_token: str):
@@ -11,6 +16,9 @@ class PviCard:
         order_number: str,
         code_prefix_list_comma_separated: str,
     ):
+        logger.info(
+            f"Syncing order {order_number} to pvicard.com server, code prefix list: {code_prefix_list_comma_separated}"
+        )
         request_url = "https://pvicard.com/api/merchant/sync-orders/api_lazada/"
 
         request_data = {
@@ -22,5 +30,8 @@ class PviCard:
             request_url,
             headers={"Authorization": f"Token {self.__api_token}"},
             data=request_data,
+        )
+        logger.info(
+            f"Done syncing order {order_number} to pvicard.com server, code prefix list: {code_prefix_list_comma_separated}"
         )
         return response
