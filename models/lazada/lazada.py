84dc62f7fdd1b8a9765abbefae018ac99ebe4c14
Khuong Tran <dbcgdhh@gmail.com>
2021-05-26 18:59:08 +0700

    Get access_token in constructor

--- models/lazada/lazada.py
+++ models/lazada/lazada.py
@@ -6,6 +6,7 @@ class Lazada:
     def __init__(self, app_key: str, app_secret: str):
         self.__app_key = app_key
         self.__app_secret = app_secret
+        self.__access_token()
 
     def __get_access_token(self):
         api_url_my = "https://api.lazada.com.my/rest"
@@ -21,18 +22,15 @@ class Lazada:
             error_message = response["message"]
             raise ValueError(f"{error_code}: {error_message}")
 
-        access_token = response["access_token"]
-        return access_token
+        self.__access_token = response["access_token"]
 
     def get_pending_orders(self):
         api_url_vn = "https://api.lazada.vn/rest"
-        access_token = self.__get_access_token()
-
         client = lazop.LazopClient(api_url_vn, self.__app_key, self.__app_secret)
         request = lazop.LazopRequest("/orders/get", "GET")
 
         request.add_api_param("status", "pending")
         request.add_api_param("created_after", "2021-01-01T01:00:00+07:00")
-        response = client.execute(request, access_token)
+        response = client.execute(request, self.__access_token)
 
         return list(response["data"]["orders"])
