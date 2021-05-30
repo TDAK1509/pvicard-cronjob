0926d99f50c17d9b63e348a5a8c62a49f8f4d7f7
Khuong Tran <dbcgdhh@gmail.com>
2021-05-30 12:17:57 +0700

    Fetch refresh token from database

--- models/lazada/pvicard.py
+++ models/lazada/pvicard.py
@@ -9,6 +9,7 @@ logger = LazadaLogger().get_logger(__name__)
 class PviCard:
     def __init__(self, api_token: str):
         self.__api_token = api_token
+        self.__base_url = "https://pvicard.com"
 
     def sync_orders(
         self,
@@ -19,7 +20,7 @@ class PviCard:
         logger.info(
             f"Syncing order {order_number} to pvicard.com server, code prefix list: {code_prefix_list_comma_separated}"
         )
-        request_url = "https://pvicard.com/api/merchant/sync-orders/api_lazada/"
+        request_url = f"{self.__base_url}/api/merchant/sync-orders/api_lazada/"
 
         request_data = {
             "email": email,
@@ -35,3 +36,14 @@ class PviCard:
             f"Done syncing order {order_number} to pvicard.com server, code prefix list: {code_prefix_list_comma_separated}"
         )
         return response
+
+    def fetch_refresh_token(self):
+        logger.info("Fetching refresh token from pvicard.com database")
+        request_url = f"{self.__base_url}/api/lazada/refresh-token/get/"
+
+        response = requests.get(
+            request_url,
+            headers={"Authorization": f"Token {self.__api_token}"},
+        )
+        logger.info("Done fetching refresh token from pvicard.com database")
+        return response.text
