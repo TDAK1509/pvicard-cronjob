834358ca0cf3c1013b0f21439f6406c15d7cd401
Khuong Tran <dbcgdhh@gmail.com>
2021-06-25 11:21:49 +0700

    Replace file handler with watchtower handler

--- models/lazada/logger.py
+++ models/lazada/logger.py
@@ -1,4 +1,4 @@
-import logging
+import boto3, logging, watchtower
 from logging.handlers import TimedRotatingFileHandler
 
 
@@ -7,13 +7,14 @@ class LazadaLogger:
     def get_logger(logger_name: str = __name__):
         logger = logging.getLogger(logger_name)
         logger.setLevel(logging.INFO)
-        logger.addHandler(LazadaLogger.__get_file_handler())
+        logger.addHandler(LazadaLogger.__get_cloudwatch_handler("private"))
         return logger
 
     @staticmethod
-    def __get_file_handler():
-        handler = TimedRotatingFileHandler(
-            "lazada.log", backupCount=1, when="D", interval=30
+    def __get_cloudwatch_handler(s3_profile_name: str = None):
+        s3 = boto3.session.Session(profile_name=s3_profile_name)
+        handler = watchtower.CloudWatchLogHandler(
+            log_group="pvicard_cronjob", boto3_session=s3
         )
         handler.setFormatter(LazadaLogger.__get_file_log_formatter())
         return handler
@@ -21,6 +22,6 @@ class LazadaLogger:
     @staticmethod
     def __get_file_log_formatter():
         formatter = logging.Formatter(
-            "%(asctime)s [%(levelname)s:%(name)s:%(lineno)d]: %(message)s"
+            "%(levelname)s [%(name)s:%(lineno)d]: %(message)s"
         )
         return formatter
